238102c7f35940aef5a7862a6ed9fca31df93eeb
gustavoblanco11 <142844294+gustavoblanco11@users.noreply.github.com>
2023-10-16 10:59:51 -0500

    Update TheAdventuresOfGroot.py
    Added comments

--- Test/TheAdventuresOfGroot.py
+++ Test/TheAdventuresOfGroot.py
@@ -1,69 +1,62 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 13 16:45:14 2023
-
-@author: gusta
-"""
 import pygame
 
-pygame.init()
+pygame.init()       #Always needed to initialize pygame
 
-S_Width = 1500
+S_Width = 1500      #Screen size in bits
 S_Height = 800
 
 screen = pygame.display.set_mode((S_Width, S_Height))
 
-groot = pygame.image.load('Grooty.png')
-desired_width = 150
-desired_height = 150
-groot = pygame.transform.scale(groot, (desired_width, desired_height))
+groot = pygame.image.load('Grooty.png') #Uploading Groot avatar 
+groot_width = 150                       #Groot size in bits
+groot_height = 150
+groot = pygame.transform.scale(groot, (groot_width, groot_height))
 
-# Raise the floor by increasing the floor_position value
-floor_position = S_Height - 100  # Adjust the value as needed
+floor_position = S_Height - 100  #Raise or lower the floor by increasing if needed
 
 player = groot.get_rect()
-player.center = (S_Width // 13, floor_position - groot.get_height() // 2)  # Start Groot above the floor
+player.center = (S_Width // 13, floor_position - groot.get_height() // 2)  #Start Groot above the floor
 
-background = pygame.image.load('background.png')
+background = pygame.image.load('background.png')    #Uploading Super Mario background
 background = pygame.transform.scale(background, (S_Width, S_Height))
 
 # Custom font for the main menu title
-menu_font = pygame.font.Font('33713_SerpentineBoldItalic.ttf', 50)  # Replace 'your_font.ttf' with the path to your chosen font file
+menu_font = pygame.font.Font('33713_SerpentineBoldItalic.ttf', 50)  #Uploading font and selecting size
 
 def main_menu():
-    epic_title = menu_font.render("The Adventures of Groot", True, (0, 0, 0))
-    quest_subtitle = menu_font.render("The quest for the Groot Norm", True, (0, 0, 0))
+    title = menu_font.render("The Adventures of Groot", True, (0, 0, 0))        #Setting Title of game
+    subtitle = menu_font.render("The quest for the Groot Norm", True, (0, 0, 0))    #Setting a subtitle for the game
     
-    title_rect = epic_title.get_rect(center=(S_Width // 2, S_Height // 4))
-    subtitle_rect = quest_subtitle.get_rect(center=(S_Width // 2, S_Height // 2))
+    title_rect = title.get_rect(center=(S_Width // 2, S_Height // 4))
+    subtitle_rect = subtitle.get_rect(center=(S_Width // 2, S_Height // 3))
 
     start_button = pygame.Rect(S_Width // 2 - 100, S_Height // 1.5, 200, 50)
     quit_button = pygame.Rect(S_Width // 2 - 100, S_Height // 1.5 + 70, 200, 50)
 
-    while True:
+    while True:     #Always boots up and displays the main menu until Start or Quit button used
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.collidepoint(event.pos):
-                    return  # Exit the main menu function to start the game
-                if quit_button.collidepoint(event.pos):
+            if event.type == pygame.MOUSEBUTTONDOWN:    #If mouse button if clicked
+                if start_button.collidepoint(event.pos):    #Checks if start was clicked
+                    return  
+                if quit_button.collidepoint(event.pos):     #Checks if quit was clicked
                     pygame.quit()
 
         screen.fill((0, 0, 0))
         screen.blit(background, (0, 0))
-        screen.blit(epic_title, title_rect)
-        screen.blit(quest_subtitle, subtitle_rect)
+        screen.blit(title, title_rect)
+        screen.blit(subtitle, subtitle_rect)
         
         pygame.draw.rect(screen, (255, 0, 0), start_button)
         start_text = menu_font.render("Start", True, (255, 255, 255))
-        start_text_rect = start_text.get_rect(center=start_button.center)  # Center the text on the button
+        start_text_rect = start_text.get_rect(center=start_button.center)  #Drawing Start button and word "Start"
         screen.blit(start_text, start_text_rect)
         
         pygame.draw.rect(screen, (0, 255, 0), quit_button)
         quit_text = menu_font.render("Quit", True, (255, 255, 255))
-        quit_text_rect = quit_text.get_rect(center=quit_button.center)  # Center the text on the button
+        quit_text_rect = quit_text.get_rect(center=quit_button.center)  #Drawing Quit button and word "Quit"
         screen.blit(quit_text, quit_text_rect)
         
         pygame.display.update()
@@ -72,40 +65,39 @@ def game_loop():
     character_speed = 2
     gravity = 1
     jumping = False
-    jump_strength = 120
+    jump = 150
 
     run = True
-    while run:
+    while run:              #Always must have a while loop with a variable that determines if the game is running or not
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_a] and not key[pygame.K_d]:
+        if key[pygame.K_a] and not key[pygame.K_d]:     #Setting up movement (left)
             player.move_ip(-character_speed, 0)
-        elif key[pygame.K_d] and not key[pygame.K_a]:
+        elif key[pygame.K_d] and not key[pygame.K_a]:   #(right)
             player.move_ip(character_speed, 0)
 
-        if key[pygame.K_SPACE] and not key[pygame.K_s] and not jumping:
+        if (key[pygame.K_SPACE] or key[pygame.K_w]) and not key[pygame.K_s] and not jumping:    #Jump
             jumping = True
-            player.move_ip(0, -jump_strength)
+            player.move_ip(0, -jump)    #y axis is inverted
 
-        player.move_ip(0, gravity)  # Apply gravity
+        player.move_ip(0, gravity)  #Apply gravity at all times
 
-        # Check for collisions with the new floor position
-        if player.y >= floor_position - player.height:
+        if player.y >= floor_position - player.height:      #Makes sure that player never goes below the floor
             jumping = False
             player.y = floor_position - player.height
         
-        if player.x == S_Width:
+        if player.x == S_Width:     #Resets position if player reaches the end of the screen
             player.x = 0
 
         screen.fill((0, 0, 0))
         screen.blit(background, (0, 0))
         screen.blit(groot, player)
-        pygame.display.update()
+        pygame.display.update()             #Refreshes the display; if not used, every moving object will leave an after image
 
-main_menu()  # Start with the epic main menu
-game_loop()  # Transition to the game loop when the player clicks "Start"
-pygame.quit()
+main_menu()  # Game opens on main menu
+game_loop()  # Transition from main menu to actual game
+pygame.quit()   #pygame.quit() always needed!!!!!!!!!!!!!!!
