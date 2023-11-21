c3190e91fe51f34a628e275f840f3a70ec732778
sid2629 <143134069+sid2629@users.noreply.github.com>
2023-11-20 21:58:06 -0600

    Update FinalGame.py
    w timer

--- Nov 15/FinalGame.py
+++ Nov 15/FinalGame.py
@@ -8,6 +8,16 @@ from queue import PriorityQueue
 resolution = (800,600)
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (400,50)
 
+def countdown_timer(window, start_time, total_time, x, y, font_color=(255, 0, 0)):
+    elapsed_time = pygame.time.get_ticks() - start_time
+    remaining_time = max(total_time - elapsed_time // 1000, 0)
+    timer_font = pygame.font.SysFont(None, 50)
+    timer_surface = timer_font.render(f'{remaining_time}s', True, (255, 0, 0))
+    timer_rect = timer_surface.get_rect()
+    timer_rect.topleft = (x, y)
+    window.blit(timer_surface, timer_rect)
+    return remaining_time
+
 def pymaze():
     global m
     m = maze(10,10)
@@ -75,7 +85,7 @@ def main_menu():
     S_Width = 800
     S_Height = 600
     screen = pygame.display.set_mode((S_Width, S_Height))
-    frame_folder = r'C:\Users\gusta\Desktop\Fall 2023\Programming\PyGame\Stick figure'
+    frame_folder = r"C:\Users\Sidne\Documents\Python Scripts\PyGame\Maze\First Attempt"
     #Load in the images to create a "gif" of sorts
     frames = []
     leftframes=[]
@@ -94,7 +104,7 @@ def main_menu():
     
     current_frame = 0
     sleep_frame = 0
-    sleep_folder = r'C:\Users\gusta\Desktop\Fall 2023\Programming\PyGame\Stick figure\sleeping'
+    sleep_folder = r"C:\Users\Sidne\Documents\Python Scripts\PyGame\Maze\First Attempt\sleeping"
     sleeps = []
     for i in range(1, 5):
         sleep_path = os.path.join(sleep_folder, f'Sleeping{i}.png')
@@ -167,21 +177,20 @@ def main_menu():
         
 
 def level1():
-    frame_folder = r'C:\Users\gusta\Desktop\Fall 2023\Programming\PyGame\Stick figure'
+    frame_folder = r"C:\Users\Sidne\Documents\Python Scripts\PyGame\Maze\First Attempt"
     #Load in the images to create a "gif" of sorts
     frames = []
     leftframes=[]
     for i in range(1, 5):
         frame_path = os.path.join(frame_folder, f'Running{i}.png')
         frame = pygame.image.load(frame_path)
-        desired_width = 100 //2
+        desired_width = 100 // 2
         desired_height = 100 // 2
         frame = pygame.transform.scale(frame, (desired_width, desired_height))
         frames.append(frame)
         leftframe = pygame.transform.flip(frame, True, False)
         leftframes.append(leftframe)
-    
-    #Load the idle image (stick standing)
+
     idle_path = os.path.join(frame_folder, 'Idle.png')
     idle_frame = pygame.image.load(idle_path)
     idle_frame = pygame.transform.scale(idle_frame, (desired_width, desired_height))
@@ -217,10 +226,15 @@ def level1():
     
     finish = pygame.Rect(7*square_size, 3 * square_size, square_size, square_size)
     
+    
+
     upgrade = False
+    start_time = pygame.time.get_ticks()
+    total_time = 60
     run = True
     speed = 5
     while run:
+
         if player.colliderect(foodrect):
             food = pygame.transform.scale(food, (0,0))
             upgrade = True
@@ -234,6 +248,8 @@ def level1():
                 run = False
         playerx,playery = player.center
         key = pygame.key.get_pressed()
+
+       
         
         new_player_rect = player.move(0, 0)
         if key[pygame.K_LEFT] and playerx > 0 and maze[playery // square_size][playerx // square_size ] == 0:
@@ -285,7 +301,16 @@ def level1():
         
         window.blit(food, foodrect)
 
+
+
+        
+
         pygame.draw.rect(window, (0, 255, 0), finish)
+        remaining_time = countdown_timer(window, start_time, total_time, 10, 10, (255,0,0))
+        if remaining_time == 0:
+            main_menu()
+            level1()
+            break
 
         pygame.display.update()
         pygame.display.flip()
@@ -293,7 +318,7 @@ def level1():
         pygame.time.Clock().tick(60)    #60=FPS
     
 def level2():
-    frame_folder = r'C:\Users\gusta\Desktop\Fall 2023\Programming\PyGame\Stick figure'
+    frame_folder = r"C:\Users\Sidne\Documents\Python Scripts\PyGame\Maze\First Attempt"
     #Load in the images to create a "gif" of sorts
     frames = []
     leftframes=[]
@@ -317,22 +342,24 @@ def level2():
     left = False
     
     maze = [ 
-    [1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1,0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1,0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1,0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [1,0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-    [1,0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1,0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1,0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
-    [1,0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1,0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1,0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-    [1,0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1,0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]
+
     ]
 
         
@@ -347,21 +374,29 @@ def level2():
     food = pygame.image.load('food.png').convert_alpha()
     food = pygame.transform.scale(food, (square_size - 10, square_size - 10))
     foodrect = food.get_rect()
-    foodrect.topleft = 1 * square_size, 4 * square_size
+    foodrect.topleft = 1 * square_size, 5 * square_size
     
     tri = pygame.image.load('red_arrow.png').convert_alpha()
     tri = pygame.transform.scale(tri, (square_size - 10, square_size - 30))
     trirect = tri.get_rect()
-    trirect.topleft = 1.15 * square_size, 1.25 * square_size
+    trirect.topleft = 1.15 * square_size, 2 * square_size
     
     finish = pygame.Rect(1*square_size, 1 * square_size, square_size, square_size)
     run = True
+    start_time = pygame.time.get_ticks()
+    total_time = 15
     speed = 5
     start = True
     while run:
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                run = False
+
+ 
         if start:
             player.topleft=(650, 700)
             start = False
+
         
         if player.colliderect(foodrect):
             food = pygame.transform.scale(food, (0,0))
@@ -373,9 +408,7 @@ def level2():
             run = False
             pygame.display.quit()
             pymaze()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+
         playerx,playery = player.center
         px = playerx // square_size
         py = playery // square_size
@@ -414,6 +447,13 @@ def level2():
                 
         
         window.fill((255,255,255))
+        remaining_time = countdown_timer(window, start_time, total_time, 10, 10, font_color=(255,0,0))
+        if remaining_time == 0:
+            print("Time's Up!")
+            main_menu()
+            level1()
+            level2()
+            break
         for y in range(len(maze)):
             for x in range(len(maze[0])):
                 if maze[y][x] == 1:
@@ -429,8 +469,8 @@ def level2():
             window.blit(idle_frame, player)  # Idle if not moving
         
             #this bellow is for level 2 !!!!!
-        pygame.draw.rect(window, (0, 0, 255), (5 * square_size, 1 * square_size, square_size, square_size)) #blue
-        pygame.draw.rect(window, (0, 0, 255), (6 * square_size, 8 * square_size, square_size, square_size)) #blue
+        pygame.draw.rect(window, (0, 0, 255), (5 * square_size, 2 * square_size, square_size, square_size)) #blue
+        pygame.draw.rect(window, (0, 0, 255), (6 * square_size, 9 * square_size, square_size, square_size)) #blue
         
         if (px == 0 and py == 3):
             px = 12
@@ -446,6 +486,7 @@ def level2():
              playery = py*square_size
              player.topleft=(playerx, playery)
         
+        
         if player.colliderect(foodrect):
             player.topleft=(650, 700)   #Trap
         if player.colliderect((5 * square_size, 1 * square_size, square_size, square_size)):
@@ -456,8 +497,10 @@ def level2():
             pymaze()
             return
         
+        
         window.blit(food, foodrect)
         window.blit(tri, trirect)
+
 
         pygame.display.update()
         pygame.display.flip()
